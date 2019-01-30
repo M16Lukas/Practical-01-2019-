62a74803a781fe718cb17c94ea6862cdab47e8b0
M16Lukas <46748131+M16Lukas@users.noreply.github.com>
2019-01-30 14:47:16 +0900

    The multiplication Table used Python

---
+++ InteralFunc/multiplication_Table.py
@@ -0,0 +1,7 @@
+# Python Program to Display the multiplication Table
+# multiplication Table : from 1 to 10
+
+num = int(input("input the number : "))
+
+for i in range(1, 11):
+    print(num, 'x', i, '=', num * i)
